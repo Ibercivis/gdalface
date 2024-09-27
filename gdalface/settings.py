51a0096c141773960440d9936e966c06a506ecf6
Francisco Sanz <frasanz@bifi.es>
2024-09-27 23:05:07 +0200

    Testing django CI

--- gdalface/settings.py
+++ gdalface/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'gdalface.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
+        'NAME': config('DB_NAME', default='gdalface'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
