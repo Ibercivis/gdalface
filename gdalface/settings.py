8729eb4a7b45c96e757454b99d4a4d16dfeab429
Francisco Sanz <frasanz@bifi.es>
2024-09-28 07:27:40 +0000

    Trying to resolve static file storage adding Manifest to settings.py

--- gdalface/settings.py
+++ gdalface/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collected static files go in production
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Your static files in development
