677df8afe9250f7539f9b322cad4d3a75a4897d4
Francisco Sanz <frasanz@bifi.es>
2024-10-18 21:53:56 +0200

    Added pylintrc, configured

--- frontend/apps.py
+++ frontend/apps.py
@@ -1,6 +1,17 @@
+"""
+### FrontendConfigq
+"""
 from django.apps import AppConfig
 
 
 class FrontendConfig(AppConfig):
+    """
+    FrontendConfig is a Django application configuration class for the 'frontend' app.
+
+    Attributes:
+        default_auto_field (str): Specifies the type of auto-incrementing 
+        primary key to use for models in this app.
+        name (str): The name of the app this configuration applies to.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'frontend'
